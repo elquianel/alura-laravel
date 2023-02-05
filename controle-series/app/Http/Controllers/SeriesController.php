01f3ef2a21c289ce6a3b678adf9bde37d0675525
Elquiane Lima <elquianelima23@gmail.com>
2023-02-05 17:38:53 -0300

    começando a utilizar models e o ORM do laravel, o eloquent

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -2,17 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
     public function index(){
-        {
-            $series = [
-                'Dark',
-                'Lost',
-                'Round 6'
-            ];
+
+        //o db não é uma boa utilização, pois acessa diretamente o banco de dados
+        // $series = DB::select('SELECT nome from series');
+
+        //maneira correta de se faazer, utilizando o ORM do laravel, o elouquent
+        $series = Serie::all();
 
             // o 'series' => nada mais é que o nome que vc irá manusear lá na view
             // return view('index', ['series' => $series]);
@@ -22,10 +24,23 @@ class SeriesController extends Controller
 
             //outra maneira e até mais expressiva de se retornar uma view e passar dados para a mesma
             return view('series.index')->with('series', $series);
-        }
     }
 
     public function create(){
         return view('series.create');
+    }
+
+    public function store(Request $request){
+        $nomeSerie = $request->input('nome');
+
+        //o db não é uma boa utilização, pois acessa diretamente o banco de dados
+        // DB::insert('INSERT INTO series (nome) VALUES (?)', [$nomeSerie]);
+
+        $serie = new Serie();
+
+        $serie->nome = $nomeSerie;
+        $serie->save();
+
+        return redirect('/series');
     }
 }
